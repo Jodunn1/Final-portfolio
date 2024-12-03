8b3c0ac2b3cf30373f622529fff0f07b1bd09887
Jordan.dunn <kidsolo33@gmail.com>
2024-12-03 13:55:35 -0800

    Fixing particles

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 particlesJS("particles-js", {
   particles: {
-    number: { value: 40, density: { enable: true, value_area: 1000 } },
+    number: { value: 10, density: { enable: true, value_area: 1000 } },
     color: { value: "#ffffff" },
     shape: {
       type: "circle",
