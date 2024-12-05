7bcd8aaea25f8ae101944ea44273f3fb2316dff9
Jordan.dunn <kidsolo33@gmail.com>
2024-12-04 21:11:28 -0800

    Finalized contact section

--- js/scripts.js
+++ js/scripts.js
@@ -147,4 +147,31 @@ document.addEventListener("DOMContentLoaded", () => {
 
 }); 
 
+document.addEventListener("DOMContentLoaded", () => {
+
+	// Use Intersection Observer to determine if objects are within the viewport
+	const observer = new IntersectionObserver(entries => {
+        const contactblue = document.querySelector('.contact-blue');
+	  entries.forEach(entry => {
+		if (entry.isIntersecting) {
+		  entry.target.classList.add('in-view');
+          contactblue.classList.add('blue');  
+		  return;
+		}
+		entry.target.classList.remove('in-view');
+        contactblue.classList.remove('blue');
+	  }
+    );
+	});
+
+	// Get all the elements with the .animate class applied
+	const allAnimatedElements = document.querySelectorAll('.contact');
+
+	// Add the observer to each of those elements
+	allAnimatedElements.forEach((element) => observer.observe(element));
+
+}); 
+
+
+
 
